6bd42b5c451cb0927ec82c1f441676bcaa1aee46
James Rees <James.rees@outlook.com>
2019-03-21 13:25:01 +0000

    Modified styling and fixed package security warnings

--- frontend/src/components/VideoPlayer.tsx
+++ frontend/src/components/VideoPlayer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'font-awesome/css/font-awesome.min.css';
 
 class VideoPlayer extends React.Component<any, any> {
 
@@ -9,25 +10,58 @@ class VideoPlayer extends React.Component<any, any> {
     }
     render() 
     {
+        let playerStyle = 
+        {
+           // display: "flex"
+           background: "white"
+        }
+
+        let videoStyle = 
+        {
+            display: "flex",
+            width: "640px",
+            height: "360px",
+            background: "black"
+        }
+
+        let groupStyle = 
+        {
+            background: "white",
+            display: "flex",
+            alignItems: "flex-end",
+            height: "25px",
+            paddingBottom: "5px",
+        }
+
+        let buttonStyle = 
+        {
+            display: "flex",
+            fontSize: "20px",
+            color: "grey",
+            border: "none", /* Remove borders */
+            background: "none",
+            cursor: "pointer"
+        }
+
+        {/* <div className="progress">
+        <progress id="progress" value="90">
+            <span id="progress-bar"></span>
+        </progress>
+    </div> */}
         return (
-            <div> 
-                <video width="640" height="360" src={this.props.video.path} className="mainVideo">
+            <div style={playerStyle}> 
+                {/* <div style={videoStyle}>
+
+                </div> */}
+                <video src={this.props.video.path} className="mainVideo" style={videoStyle}>
                     <track kind="subtitles" src="test.vtt" label="English" srcLang="en" default />
                     <track kind="subtitles" src="test2.vtt" label="Spanish" srcLang="es" />
                 </video>
-                <div id="video-controls" className="controls" data-state="hidden">
-                    <button id="play" type="button" data-state="play" onClick={this.play}>Play</button>
-                    <button id="pause" type="button" data-state="play" onClick={this.pause}>Pause</button>
-                    <button id="stop" type="button" data-state="stop">Stop</button>
-                    <div className="progress">
-                        <progress id="progress" value="0">
-                            <span id="progress-bar"></span>
-                        </progress>
-                    </div>
-                    <button id="mute" type="button" data-state="mute">Mute/Unmute</button>
-                    <button id="volinc" type="button" data-state="volup">Vol+</button>
-                    <button id="voldec" type="button" data-state="voldown">Vol-</button>
-                    <button id="fs" type="button" data-state="go-fullscreen">Fullscreen</button>
+                <div id="video-controls" className="controls" data-state="hidden" style={groupStyle}>
+                    <button id="play" type="button" data-state="play" onClick={this.play} className="fa fa-play" style={buttonStyle}></button>
+                    <button id="pause" type="button" data-state="play" onClick={this.pause} className="fa fa-pause" style={buttonStyle}></button>
+                    <button id="stop" type="button" data-state="stop" className="fa fa-stop" style={buttonStyle}></button>
+                    <button id="fs" type="button" data-state="go-fullscreen" className="fa fa-expand" style={buttonStyle}></button>
                 </div>
             </div>
         );
